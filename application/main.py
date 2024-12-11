ddd5e4bb3bfe54333aeef065afd1fcde5632859f
Camilla <camillamariasi18@gmail.com>
2024-12-11 17:20:25 -0300

    feat: consumer

--- application/main.py
+++ application/main.py
@@ -4,25 +4,22 @@ import json
 class Queues_Data_Geolocated(stomp.ConnectionListener):
 
     def on_message(self, data):
-        print('\n\nNovo dado recebido:\n\n', data.body)
         self.process_data(data.body)
 
     def process_data(self, data):
         data = json.loads(data)
         formatted_data = {
-            "indice": data['indice'],
-            "name": data['name'],
-            "type": data['type'],
-            "latitude": data['latitude'],
-            "longitude": data['longitude'],
-            "location": data['location'],
-            "wikipedia_Link": data['wikipedia_Link'],
-            "picture_Link": data['picture_Link'],
-            "build_Year": data['build_Year'],
+            "indice": data['Indice'],
+            "name": data['Name'],
+            "type": data['Type'],
+            "latitude": data['Latitude'],
+            "longitude": data['Longitude'],
+            "location": data['Location'],
+            "WikipediaLink": data['WikipediaLink'],
+            "PictureLink": data['PictureLink'],
+            "BuildInYear": data['BuildInYear'],
         }
-
-    def close_connection(self):
-        self.connect_ActiveMQ.disconnect()
+        print(formatted_data)
 
 class Queues_Data_Tips(stomp.ConnectionListener):
 
@@ -31,30 +28,20 @@ class Queues_Data_Tips(stomp.ConnectionListener):
         self.process_data(data.body)
 
     def process_data(self, data):
-        data = json.loads(data)
-        formatted_data = {
-            "muralha_China": data['muralha_China'],
-            "petra": data['petra'],
-            "cristo_Redentor": data['cristo_Redentor'],
-            "machu_Picchu": data['machu_Picchu'],
-            "chichen_Itza": data['chichen_Itza'],
-            "coliseu": data['coliseu'],
-            "taj_Mahal": data['taj_Mahal'],
-        }
+        tips_List = json.loads(data)
 
-
-    def close_connection(self):
-        self.connect_ActiveMQ.disconnect()
 
 connection_Data_Geolocated = stomp.Connection([('localhost', 61613)])  
 connection_Data_Geolocated.set_listener('', Queues_Data_Geolocated())
 connection_Data_Geolocated.connect(wait=True)
 connection_Data_Geolocated.subscribe(destination='/queue/data_geolocated', id=1, ack='auto')
-connection_Data_Geolocated.close_connection()
+connection_Data_Geolocated.disconnect()
 
 connection_Data_Tips = stomp.Connection([('localhost', 61613)])  
 connection_Data_Tips.set_listener('', Queues_Data_Tips())
 connection_Data_Tips.connect(wait=True)
 connection_Data_Tips.subscribe(destination='/queue/data_tips', id=1, ack='auto')
-connection_Data_Tips.close_connection()
+connection_Data_Tips.disconnect()
 
+while True:
+    pass
